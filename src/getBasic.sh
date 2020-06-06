6653234975f51ed4d52fad38f22f132ce2116628
Sandesh Shrestha <mail@sandeshshrestha.com>
2020-06-06 23:37:58 +0200

    Fixes #4, Small optimization

--- src/getBasic.sh
+++ src/getBasic.sh
@@ -5,24 +5,30 @@ PROXY=$2
 
 echo "
 server {
-	listen 80;
-	server_name $URL;
-	client_max_body_size 10G;
+	listen               80;
+	server_name          $URL;
 
 	location /.well-known {
 		auth_basic off;
-		alias /var/www/$URL/.well-known;
+		alias      /var/www/$URL/.well-known;
 	}
 
-	access_log /var/log/nginx/$URL.access.log;
-	error_log /var/log/nginx/$URL.error.log;
+	error_log /var/log/nginx/$URL.error.log crit;
 
 	location / {
-		proxy_set_header Host \$host;
-		proxy_set_header X-Real-IP \$remote_addr;
-		proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-		proxy_set_header X-Forwarded-Proto \$scheme;
-		proxy_pass $PROXY;
+    proxy_redirect   off;
+    proxy_set_header Host \$host;
+    proxy_set_header X-Real-IP $remote_addr;
+		proxy_pass       $PROXY;
+    proxy_buffering    off;
+    proxy_buffer_size  128k;
+    proxy_buffers 100  128k;
+
+    location ~* \.(?:ico|css|js|gif|jpe?g|png)$ {
+      expires 120d;
+      add_header Pragma public;
+      add_header Cache-Control \"public\";
+    }
 	}
 }
 ";
--- src/getExtended.sh
+++ src/getExtended.sh
@@ -14,9 +14,30 @@ server {
 server {
 	listen 443 ssl http2;
 	listen [::]:443 ssl http2;
-
 	server_name $URL;
-	client_max_body_size 10G;
+
+	location /.well-known {
+		auth_basic off;
+		alias /var/www/$URL/.well-known;
+	}
+
+	error_log /var/log/nginx/$URL.error.log crit;
+
+	location / {
+    proxy_redirect   off;
+    proxy_set_header Host \$host;
+    proxy_set_header X-Real-IP \$remote_addr;
+		proxy_pass       $PROXY;
+    proxy_buffering    off;
+    proxy_buffer_size  128k;
+    proxy_buffers 100  128k;
+
+    location ~* \.(?:ico|css|js|gif|jpe?g|png)$ {
+      expires 120d;
+      add_header Pragma public;
+      add_header Cache-Control \"public\";
+    }
+	}
 
 	chunked_transfer_encoding on;
 
@@ -25,11 +46,6 @@ server {
 	ssl_session_timeout 1d;
 	ssl_session_cache shared:SSL:50m;
 	ssl_session_tickets off;
-
-	access_log /var/log/nginx/$URL.access.log;
-	error_log /var/log/nginx/$URL.error.log;
-
-	$EXTRA_CONFIG
 
 	ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
 	ssl_ciphers 'ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:DES-CBC3-SHA:!DSS';
@@ -45,17 +61,6 @@ server {
 	resolver 1.1.1.1 valid=300s;
 	resolver_timeout 5s;
 
-	location /.well-known {
-		auth_basic off;
-		alias /var/www/$URL/.well-known;
-	}
-
-	location / {
-		proxy_set_header Host \$host;
-		proxy_set_header X-Real-IP \$remote_addr;
-		proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-		proxy_set_header X-Forwarded-Proto \$scheme;
-		proxy_pass $PROXY;
-	}
+	$EXTRA_CONFIG
 }
 "
